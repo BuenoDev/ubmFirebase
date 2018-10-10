21311d91e6bfe1572390387b4b53cccb9f9aeba8
gustavo <gbueno360@gmail.com>
2018-10-09 21:25:53 -0300

    initial commit

---
+++ app/src/main/java/com/example/aluno/firebase/FormListAdapter.java
@@ -0,0 +1,51 @@
+package com.example.aluno.firebase;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class FormListAdapter extends RecyclerView.Adapter<FormListAdapter.FormViewHolder> {
+
+    DatabaseReference database;
+
+    @NonNull
+    @Override
+    public FormViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_layout,parent,false);
+        database = FirebaseDatabase.getInstance().getReference();
+
+        return new FormViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull FormViewHolder holder, int position) {
+        //Dados vindos do Firebase
+        String nome = database.child("Usuario").child("Nome").toString();
+        String email = database.child("Usuario").child("Email").toString();
+    }
+
+    @Override
+    public int getItemCount() {
+        return 10;
+    }
+
+    public class FormViewHolder extends RecyclerView.ViewHolder{
+        TextView textNome;
+        TextView textEmail;
+        public FormViewHolder(@NonNull  View v){
+            super(v);
+
+            textNome = v.findViewById(R.id.textViewNome);
+            textEmail = v.findViewById(R.id.textViewEmail);
+        }
+    }
+
+
+}
---
+++ app/src/main/java/com/example/aluno/firebase/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.aluno.firebase;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+
+    DatabaseReference database;
+
+    EditText editNome;
+    EditText editEmail;
+    Button btn;
+
+    RecyclerView recyclerView;
+    RecyclerView.Adapter viewAdapter;
+    RecyclerView.LayoutManager viewManager;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        database = FirebaseDatabase.getInstance().getReference();
+
+        editNome = findViewById(R.id.editNome);
+        editEmail = findViewById(R.id.editEmail);
+        btn = findViewById(R.id.btn);
+
+        recyclerView = findViewById(R.id.recyclerView);
+
+        recyclerView.setHasFixedSize(true);
+
+        viewAdapter = new FormListAdapter();
+        recyclerView.setAdapter(viewAdapter);
+
+        viewManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(viewManager);
+
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String nome = editNome.getText().toString();
+                String email = editEmail.getText().toString();
+
+                database.child("Usuario").child("Nome").child(nome);
+                database.child("Usuario").child("Email").child(email);
+            }
+        });
+    }
+}
